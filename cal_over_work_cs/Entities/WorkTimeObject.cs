e5e923c2e05f89b150ab014ba047116d3ea33c18
kevin.tung <chen0625tung@gmail.com>
2021-09-18 11:50:11 +0800

    modified calculation of worktime:

---
+++ cal_over_work_cs/Entities/WorkTimeObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace cal_over_work_cs.Entities
+{
+    public class WorkTimeObject
+    {
+        public WorkTimeObject(string date, string arriveTime, string leaveTime)
+        {
+            this.Date = date;
+            this.ArriveTime = arriveTime;
+            this.LeaveTime = leaveTime;
+        }
+
+        public string Date { get; set; }
+        public string ArriveTime { get; set; }
+        public string LeaveTime { get; set; }
+        public bool IsNormalWorkDay { get; set; }
+
+        public string Month => Date[..2];
+        public string Day => Date[2..];
+    }
+}
--- cal_over_work_cs/Program.cs
+++ cal_over_work_cs/Program.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using cal_over_work_cs.Entities;
 using NPOI.SS.UserModel;
 using NPOI.XSSF.UserModel;
 
 namespace cal_over_work_cs
 {
     class Program
-    { 
+    {
+        private const string Yes = "Y";
         private const string WorkStartHours = "09:00";
         private const string WorkEndHours = "18:30";
         private static string _month = "";
@@ -29,7 +31,8 @@ namespace cal_over_work_cs
 
         private static void ExportToExcel(List<string> dataList, int totalOverWorkedHours)
         {
-            string path = $"./output/workTime_output_{_month}.xlsx";
+            if(Directory.Exists($"./output/{_month}/")) Directory.CreateDirectory($"./output/{_month}/");
+            string path = $"./output/{_month}/workTime_output_{_month}.xlsx";
 
             using (FileStream fs = new FileStream(path, FileMode.Create, FileAccess.Write))
             {
@@ -69,18 +72,24 @@ namespace cal_over_work_cs
             foreach (string s in workHours)
             {
                 string[] data = s.Split(", ");
-                string month = data[0].Substring(0, 2);
-                string day = data[0].Substring(2);
-                DateTime startTime = DateTime.ParseExact($"{month}-{day} {WorkStartHours}", "MM-dd HH:mm", null);
-                DateTime endTime = DateTime.ParseExact($"{month}-{day} {WorkEndHours}", "MM-dd HH:mm", null);
-                DateTime arriveTime = DateTime.ParseExact($"{month}-{day} {data[1]}", "MM-dd HH:mm", null);
-                DateTime leaveTime = DateTime.ParseExact($"{month}-{day} {data[2]}", "MM-dd HH:mm", null);
+                var workTimeObject = new WorkTimeObject(data[0], data[1], data[2]);
+                if (data.Length > 3)
+                {
+                    workTimeObject.IsNormalWorkDay = Yes.Equals(data[3]);
+                }
+
+                string dataStringForExcel = $"{workTimeObject.Date}, {workTimeObject.ArriveTime}, {workTimeObject.LeaveTime}";
+                DateTime startTime = DateTime.ParseExact($"{workTimeObject.Month}-{workTimeObject.Day} {WorkStartHours}", "MM-dd HH:mm", null);
+                DateTime endTime = DateTime.ParseExact($"{workTimeObject.Month}-{workTimeObject.Day} {WorkEndHours}", "MM-dd HH:mm", null);
+                DateTime arriveTime = DateTime.ParseExact($"{workTimeObject.Month}-{workTimeObject.Day} {workTimeObject.ArriveTime}", "MM-dd HH:mm", null);
+                DateTime leaveTime = DateTime.ParseExact($"{workTimeObject.Month}-{workTimeObject.Day} {workTimeObject.LeaveTime}", "MM-dd HH:mm", null);
                 
-                OutputToConsoleAndwWriteToFile($"date: {data[0]}", outputDataList);
+                OutputToConsoleAndwWriteToFile($"date: {workTimeObject.Date}", outputDataList);
                 OutputToConsoleAndwWriteToFile($"actual arrive time: {arriveTime:yyyy-MM-dd HH:mm}", outputDataList);
                 OutputToConsoleAndwWriteToFile($"actual leave time: {leaveTime:yyyy-MM-dd HH:mm}", outputDataList);
-                if (arriveTime.DayOfWeek == DayOfWeek.Saturday)
+                if (arriveTime.DayOfWeek == DayOfWeek.Saturday && !workTimeObject.IsNormalWorkDay)
                 {
+                    // weekend calculation
                     OutputToConsoleAndwWriteToFile("isSaturday", outputDataList);
                     TimeSpan timeDiff = leaveTime - arriveTime;
                     OutputToConsoleAndwWriteToFile($"overworked in minutes: {timeDiff.TotalMinutes}", outputDataList);
@@ -88,11 +97,16 @@ namespace cal_over_work_cs
                     totalOverWorkHours += (int) Math.Floor(timeDiff.TotalHours);
                     OutputToConsoleAndwWriteToFile("\n", outputDataList);
                     string timeDiffStr = Math.Floor(timeDiff.TotalHours) > 0 ? $"+{Math.Floor(timeDiff.TotalHours)}" : $"{Math.Floor(timeDiff.TotalHours)}";
-                    dataListForExcel.Add($"{s}, {timeDiffStr}");
+                    dataListForExcel.Add($"{dataStringForExcel}, {timeDiffStr}");
                 }
                 else
                 {
+                    // normal workday calculation
                     TimeSpan differenceArriveTime = arriveTime - startTime;
+                    if (arriveTime.Hour < 9)
+                    {
+                        differenceArriveTime = TimeSpan.Zero;
+                    }
                     DateTime actualLeaveTime = endTime.Add(differenceArriveTime);
                     TimeSpan overWorked = leaveTime - actualLeaveTime;
                     
@@ -102,18 +116,19 @@ namespace cal_over_work_cs
                     totalOverWorkHours += (int) Math.Floor(overWorked.TotalHours);
                     OutputToConsoleAndwWriteToFile("\n", outputDataList);
                     string timeDiffStr = Math.Floor(overWorked.TotalHours) > 0 ? $"+{Math.Floor(overWorked.TotalHours)}" : $"{Math.Floor(overWorked.TotalHours)}";
-                    dataListForExcel.Add($"{s}, {timeDiffStr}");
+                    dataListForExcel.Add($"{dataStringForExcel}, {timeDiffStr}");
                 }
             }
             OutputToConsoleAndwWriteToFile($"total over worked hours: {totalOverWorkHours}", outputDataList);
             
             // write to file
-            File.WriteAllLines($"./output/workTime_output_{_month}.txt", outputDataList.ToArray());
+            if(!Directory.Exists($"./output/{_month}/")) Directory.CreateDirectory($"./output/{_month}/");
+            File.WriteAllLines($"./output/{_month}/workTime_output_{_month}.txt", outputDataList.ToArray());
             
             // output to Excel
             ExportToExcel(dataListForExcel, totalOverWorkHours);
             
-            Console.WriteLine("Done!");
+            Console.WriteLine("Done exporting to txt and xlsx, press any key to exit...");
             Console.ReadKey();
         }
         
